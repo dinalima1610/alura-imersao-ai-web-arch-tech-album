46152ea5a64ccfafc4a51c4d360aea3cfaca1df6
Diná Lima <dinalima.dal@gmail.com>
2026-07-16 21:45:37 -0300

    fix (frontend): aponta a base url para o ambiente de produção

--- frontend/app.js
+++ frontend/app.js
@@ -10,7 +10,7 @@ const isLocalhost = window.location.hostname === "localhost" || window.location.
 // Define a API com base no ambiente atual
 const API_BASE_URL = isLocalhost
     ? "http://localhost:8000"
-    : "https://alura-imersao-ai-web-arch-tech-album.vercel.app/";
+    : "https://alura-imersao-ai-web-arch-tech-album.vercel.app";
 
 // ===================================================
 // FUNÇÃO: Preenche os slots do álbum com imagens da API
